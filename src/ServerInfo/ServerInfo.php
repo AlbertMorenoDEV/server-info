6b1f7a0cdbbd9c5685f2132c813f1440f26df8ef
albert.moreno <albert.moreno.dev@gmail.com>
2016-10-16 16:25:43 +0200

    Added disk total space

--- src/ServerInfo/ServerInfo.php
+++ src/ServerInfo/ServerInfo.php
@@ -4,6 +4,11 @@ namespace ServerInfo;
 
 class ServerInfo
 {
+    public static function disk_total_space($directory = "/")
+    {
+        return disk_total_space($directory);
+    }
+
     public static function disk_free_space($directory = "/")
     {
         return disk_free_space($directory);
