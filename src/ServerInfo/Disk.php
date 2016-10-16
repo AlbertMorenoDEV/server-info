8af2d2b90d9598b2531dbd60003409f5b975e22b
Albert Moreno <albertmorenovendrell@gmail.com>
2016-10-16 16:29:50 +0200

    ServerInfo renamed to Disk, and changed static methods

---
+++ src/ServerInfo/Disk.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ServerInfo;
+
+class Disk
+{
+    private $path;
+
+    public function __construct($path = "/")
+    {
+        $this->path = $path;
+    }
+
+    public function totalSpace()
+    {
+        return disk_total_space($this->path);
+    }
+
+    public function freeSpace()
+    {
+        return disk_free_space($this->path);
+    }
+}
